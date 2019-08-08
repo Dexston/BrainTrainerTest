863554e328e2162df8ad01b9ee5a4a3b154c6da0
Dexston <mrdexston@gmail.com>
2019-08-08 12:37:06 +0300

    no message

--- MainActivity.java
+++ MainActivity.java
@@ -18,7 +18,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewTimer, textViewRiddle, textViewCounter;
-    private ArrayList<TextView> buttons = new ArrayList<>();
+    private ArrayList<TextView> buttons = new ArrayList<>();    //Список для кнопок
     private int answersCounter, correctAnswersCounter, numberOfRightAnswer, rightAnswer, symbol;
     private int min = 5;	//Диапазон чисел от min
     private int max = 30;	//до max
